ebd85baa02340fdea042530dc0d3497393675ddb
soyakkumarkp <soyalkumarkp@gmail.com>
2023-10-28 20:33:43 +0530

    installed typescript

---
+++ sample.js
@@ -0,0 +1,8 @@
+// npm i -g typescript
+//tsc .\sample.ts -w
+var a = "Hello world";
+console.log(a);
+
+var b = 100;
+var c = 200;
+console.log(b + c);
---
+++ sample.ts
@@ -0,0 +1,8 @@
+// npm i -g typescript
+//tsc .\sample.ts -w
+const a = "Hello world";
+console.log(a);
+
+const b = 100;
+const c = 200;
+console.log(b + c);
